8cb35485bcf40b2c55a66c22eb283704e4b301d1
hari wibowo <wibowosub@gmail.com>
2022-11-16 10:00:22 +0700

    first commit

---
+++ formphp.php
@@ -0,0 +1,29 @@
+<?php
+echo "NIK: ". $_POST['nik'];
+echo "<br>";
+echo "Nama: ". $_POST['nama'];
+echo "<br>";
+echo "Tanggal: ". $_POST['tanggal'];
+echo "<br>";
+echo "Jenis Kelamin: ". $_POST['jenisKelamin'];
+echo "<br>";
+echo "No Telp/HP: ". $_POST['hp'];
+echo "<br>";
+echo "Alamat: ". $_POST['alamat'];
+echo "<br>";
+echo "Domisili: ". $_POST['domisili'];
+echo "<br>";
+echo "Email: ". $_POST['email'];
+echo "<br>";
+echo "Password: ". $_POST['passw'];
+echo "<br>";
+echo "Konfirmasi : ". $_POST['passw2'];
+echo "<br>";
+echo "Pemeriksaan: ". $_POST['pemeriksaan'];
+echo "<br>";
+
+?>
+
+
+
+
